416e51fcd4c3ef234d2c682215828cf17b28b42d
Umberto Loria <umberto.loria@gmail.com>
2024-08-14 01:04:06 +0200

    Improved mapping between input and output files

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 use color_manipulator_rust::RGB;
 use image::{GenericImageView, ImageBuffer, Rgb};
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::{fs, io};
-
-fn calculate_color(c: RGB) -> RGB {
-    return RGB {
-        r: c.r * 0.5,
-        g: c.g * 0.5,
-        b: c.b * 0.5,
-    };
-}
 
 fn get_file_paths(dir: &str) -> Result<Vec<PathBuf>, io::Error> {
     let mut paths: Vec<PathBuf> = Vec::new();
@@ -22,37 +15,73 @@ fn get_file_paths(dir: &str) -> Result<Vec<PathBuf>, io::Error> {
     Ok(paths)
 }
 
+struct SourceFileConf {
+    files: Vec<FinalFileConf>,
+}
+struct FinalFileConf {
+    name: String,
+    calculate_color: fn(rgba: RGB) -> RGB,
+}
+
 fn main() -> Result<(), image::ImageError> {
-    let dir_path = "./input";
-    let file_paths = get_file_paths(dir_path)?;
+    let mut list_source_file_conf: HashMap<&str, SourceFileConf> = HashMap::new();
+    list_source_file_conf.insert("20230301_224920.jpg", SourceFileConf {
+        files: vec!(
+            FinalFileConf {
+                name: String::from("output.png"),
+                calculate_color: |c| {
+                    return RGB {
+                        r: c.r * 0.5,
+                        g: c.g * 0.5,
+                        b: c.b * 0.5,
+                    };
+                },
+            },
+        ),
+    });
 
-    for path in file_paths {
-        let img = image::open(path)?;
+    let files = get_file_paths("./input")?;
 
-        let mut output_img = ImageBuffer::new(img.width(), img.height());
+    for file in files {
+        let file_path_os_str = file.file_name().unwrap();
+        let file_path_string = file_path_os_str.to_string_lossy().to_string();
+        let file_path_str = &file_path_string[..];
 
-        for (x, y, pixel) in img.pixels() {
-            let src_r = *(pixel.0.get(0).unwrap());
-            let src_g = *(pixel.0.get(1).unwrap());
-            let src_b = *(pixel.0.get(2).unwrap());
+        let source_file_conf_opt = list_source_file_conf.get(file_path_str);
+        if source_file_conf_opt.is_some() {
+            let source_file_conf = source_file_conf_opt.unwrap();
 
-            // Calculate color
-            let in_color = RGB {
-                r: src_r as f32 / 255.0,
-                g: src_g as f32 / 255.0,
-                b: src_b as f32 / 255.0,
-            };
-            let out_color: RGB = calculate_color(in_color);
+            let img = image::open(&file)?;
 
-            // Write destination color
-            output_img.put_pixel(x, y, Rgb([
-                (out_color.r * 255.0) as u8,
-                (out_color.g * 255.0) as u8,
-                (out_color.b * 255.0) as u8,
-            ]));
+            for final_file_conf in &source_file_conf.files {
+                let mut output_img = ImageBuffer::new(img.width(), img.height());
+
+                for (x, y, pixel) in img.pixels() {
+                    let src_r = *(pixel.0.get(0).unwrap());
+                    let src_g = *(pixel.0.get(1).unwrap());
+                    let src_b = *(pixel.0.get(2).unwrap());
+
+                    // Calculate color
+                    let in_color = RGB {
+                        r: src_r as f32 / 255.0,
+                        g: src_g as f32 / 255.0,
+                        b: src_b as f32 / 255.0,
+                    };
+                    let final_file_conf_fn = final_file_conf.calculate_color;
+                    let out_color: RGB = final_file_conf_fn(in_color);
+
+                    // Write destination color
+                    output_img.put_pixel(x, y, Rgb([
+                        (out_color.r * 255.0) as u8,
+                        (out_color.g * 255.0) as u8,
+                        (out_color.b * 255.0) as u8,
+                    ]));
+                }
+
+                let output_file_path = format!("out/{}", final_file_conf.name);
+                output_img.save(output_file_path)?;
+            }
         }
-
-        output_img.save("out/output.png")?;
     }
 
     Ok(())

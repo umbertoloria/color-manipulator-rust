411ce4cb1f837b8912025959172102bb64f41284
Umberto Loria <umberto.loria@gmail.com>
2025-02-01 03:55:09 +0100

    Last real image converted in PNG

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use crate::projects::img_20250201_0220::img_20250201_0220;
-use crate::projects::img_diffs_create::img_diffs_create;
+use crate::projects::img_20240714_1958::img_20240714_1958;
 
 mod coord;
 mod folding;
@@ -14,8 +13,9 @@ fn main() {
     img_20230301_225057();
     img_20231002_103537_three_images();
     img_20241009_161110();
-    img_20240714_1958();
     */
-    img_20250201_0220();
-    img_diffs_create();
+    img_20240714_1958();
+
+    // img_20250201_0220();
+    // img_diffs_create();
 }
--- src/projects/img_20240714_1958.rs
+++ src/projects/img_20240714_1958.rs
@@ -1,14 +1,13 @@
-use crate::coord::get_coord_chunks;
-use crate::folding::{get_path_in, get_path_out};
+use crate::folding::get_path_in;
+use crate::int::chunking_layer::split_layer_in_chunks_and_save_parts_and_combined;
 use crate::int::int_color::rgb_to_color;
-use crate::int::int_img_buffer::IntImgBuffer;
 use crate::int::int_layer::{
-    create_int_filtered_layer, load_int_file_image_layer_box, IntAbsLayer, IntMergeLayer,
+    create_int_filtered_layer, load_int_file_image_layer_box, IntAbsLayer,
 };
 use color_manipulator_rust::{add_list, color, filter_scalar_and_stretch, get_r};
 
 pub fn img_20240714_1958() {
-    let filename = "20240714_1958.jpg";
+    let filename = "20240714_1958.png";
 
     let filtered_layer = create_int_filtered_layer(
         load_int_file_image_layer_box(&get_path_in(filename)),
@@ -25,35 +24,5 @@ pub fn img_20240714_1958() {
         },
     );
     let abs_layer: &dyn IntAbsLayer = &filtered_layer;
-    // abs_layer.save(output_file_path); // Avoid calculating the entire image.
-
-    // Separated chunks
-    let width = abs_layer.width();
-    let height = abs_layer.height();
-    let chunks_count = 4;
-    let coord_chunks = get_coord_chunks(chunks_count, width, height);
-    let mut i = 0;
-    for coord_chunk in coord_chunks {
-        let mut img_buffer_chunk = IntImgBuffer::new(
-            coord_chunk.get_width(),
-            coord_chunk.get_height(),
-            format!("{}-{}.jpg", get_path_out(filename), i),
-        );
-        img_buffer_chunk.paint(|x, y| {
-            abs_layer.get_color(coord_chunk.top_left.x + x, coord_chunk.top_left.y + y)
-        });
-        img_buffer_chunk.save();
-        i += 1;
-    }
-
-    // Merge chunks
-    let mut filepaths = Vec::new();
-    for i in 0..chunks_count {
-        filepaths.push(format!("{}-{}.jpg", get_path_out(filename), i));
-    }
-    let merge_layer = IntMergeLayer::new(filepaths);
-    let output_file_path = format!("{}_new.jpg", filename);
-    // let output_file_path = "20241009_161110.jpg";
-    let abs_layer: &dyn IntAbsLayer = &merge_layer;
-    abs_layer.save(&output_file_path);
+    split_layer_in_chunks_and_save_parts_and_combined(abs_layer, &filename);
 }

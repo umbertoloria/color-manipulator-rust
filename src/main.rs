7b4106d30483ce43ddec4a99db7aa56aa03e23a5
Umberto Loria <umberto.loria@gmail.com>
2025-02-01 04:37:15 +0100

    Third image project updated

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::projects::img_20230301_224920_two_images::img_20230301_224920_two_images;
+use crate::projects::img_20230301_225057::img_20230301_225057;
 
 mod coord;
 mod folding;
@@ -7,10 +7,12 @@ mod layer;
 pub mod projects;
 
 fn main() {
-    // img_20230226_201501();
-    img_20230301_224920_two_images();
     /*
+    img_20230226_201501();
+    img_20230301_224920_two_images();
+    */
     img_20230301_225057();
+    /*
     img_20231002_103537_three_images();
     img_20241009_161110();
     */
--- src/projects/img_20230301_225057.rs
+++ src/projects/img_20230301_225057.rs
@@ -1,13 +1,16 @@
-use crate::layer::{create_filtered_layer, load_image_layer};
+use crate::folding::get_path_out;
+use crate::int::int_color::rgb_to_color;
+use crate::int::int_layer::{create_int_filtered_layer, load_int_file_image_layer_box};
 use color_manipulator_rust::{
     add, color, filter_scalar_and_stretch, get_b, get_g, get_r, mult, scalar,
 };
 
 pub fn img_20230301_225057() {
-    create_filtered_layer(
-        Box::new(load_image_layer("./input/20230301_225057.jpg")),
+    create_int_filtered_layer(
+        load_int_file_image_layer_box("./input/20230301_225057.jpg"),
         |c, _p| {
-            add(
+            let c = c.to_rgb();
+            rgb_to_color(add(
                 color(0.0, 0.2, 0.15),
                 add(
                     mult(
@@ -25,8 +28,8 @@ pub fn img_20230301_225057() {
                         ),
                     ),
                 ),
-            )
+            ))
         },
     )
-    .save("20230303_162133.jpg");
+    .save(get_path_out("20230303_162133.jpg"));
 }

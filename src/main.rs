2b4e5bcccd40b61b784a3773ac295c705019773a
Umberto Loria <umberto.loria@gmail.com>
2025-02-01 04:30:38 +0100

    Second image project updated

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use crate::projects::img_20230226_201501::img_20230226_201501;
-use crate::projects::img_20240714_1958::img_20240714_1958;
+use crate::projects::img_20230301_224920_two_images::img_20230301_224920_two_images;
 
 mod coord;
 mod folding;
@@ -8,9 +7,9 @@ mod layer;
 pub mod projects;
 
 fn main() {
-    img_20230226_201501();
-    /*
+    // img_20230226_201501();
     img_20230301_224920_two_images();
+    /*
     img_20230301_225057();
     img_20231002_103537_three_images();
     img_20241009_161110();
--- src/projects/img_20230301_224920_two_images.rs
+++ src/projects/img_20230301_224920_two_images.rs
@@ -1,31 +1,35 @@
-use crate::layer::{create_filtered_layer, load_image_layer};
+use crate::folding::get_path_out;
+use crate::int::int_color::rgb_to_color;
+use crate::int::int_layer::{create_int_filtered_layer, load_int_file_image_layer_box};
 use color_manipulator_rust::{add, color, filter_scalar, get_r, mult, only_b, scalar, xor_color};
 
 pub fn img_20230301_224920_two_images() {
-    create_filtered_layer(
-        Box::new(load_image_layer("./input/20230301_224920.jpg")),
+    create_int_filtered_layer(
+        load_int_file_image_layer_box("./input/20230301_224920.jpg"),
         |c, _p| {
-            add(
+            let c = c.to_rgb();
+            rgb_to_color(add(
                 mult(
                     scalar(filter_scalar(get_r(c), 0.6, 0.85) - 0.6),
                     color(10.0, 5.0, 0.0),
                 ),
                 only_b(0.35),
-            )
+            ))
         },
     )
-    .save("20230301_224920_1.jpg");
-    create_filtered_layer(
-        Box::new(load_image_layer("./input/20230301_224920.jpg")),
+    .save(get_path_out("20230301_224920_1.jpg"));
+    create_int_filtered_layer(
+        load_int_file_image_layer_box("./input/20230301_224920.jpg"),
         |c, _p| {
-            xor_color(
+            let c = c.to_rgb();
+            rgb_to_color(xor_color(
                 mult(
                     scalar(0.4 - filter_scalar(get_r(c), 0.35, 0.4)),
                     color(7.0, 10.0, 5.0),
                 ),
                 only_b(0.35),
-            )
+            ))
         },
     )
-    .save("20230301_224920_2.jpg");
+    .save(get_path_out("20230301_224920_2.jpg"));
 }

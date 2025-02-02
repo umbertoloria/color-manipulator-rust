7713e2fe389498547111bc49e49affbd03d35f67
Umberto Loria <umberto.loria@gmail.com>
2025-02-02 21:29:55 +0100

    ImgBuffer removed

--- src/coord.rs
+++ src/coord.rs
@@ -1,7 +1,5 @@
-use color_manipulator_rust::{POS, RGB};
-use image::{ImageBuffer, Rgb};
+use color_manipulator_rust::POS;
 
-// Coord
 #[derive(Clone, Copy, Debug)]
 pub struct Coord {
     pub x: usize,
@@ -30,54 +28,6 @@ impl CoordSquare {
     }
 }
 
-// Img Buffer
-pub struct ImgBuffer {
-    width: u32,
-    height: u32,
-    filepath: String,
-    image_buffer: Option<ImageBuffer<Rgb<u8>, Vec<u8>>>,
-}
-impl ImgBuffer {
-    pub fn new(width: u32, height: u32, filepath: String) -> Self {
-        Self {
-            width,
-            height,
-            filepath,
-            image_buffer: None,
-        }
-    }
-    pub fn paint(&mut self, fn_get_color: impl Fn(usize, usize) -> RGB) {
-        let mut image_buffer: ImageBuffer<Rgb<u8>, Vec<u8>> =
-            ImageBuffer::new(self.width, self.height);
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let out_color: RGB = fn_get_color(x as usize, y as usize);
-                image_buffer.put_pixel(
-                    x,
-                    y,
-                    Rgb([
-                        (out_color.r * 255.0) as u8,
-                        (out_color.g * 255.0) as u8,
-                        (out_color.b * 255.0) as u8,
-                    ]),
-                );
-            }
-        }
-        self.image_buffer = Some(image_buffer);
-    }
-    pub fn save(&self) {
-        if self.image_buffer.is_some() {
-            // TODO: Avoid clone
-            self.image_buffer
-                .clone()
-                .unwrap()
-                .save(self.filepath.clone())
-                .unwrap();
-        }
-    }
-}
-
-// Separation of CoordSquare
 pub fn get_coord_chunks(chunks_count: usize, width: usize, height: usize) -> Vec<CoordSquare> {
     let mut result: Vec<CoordSquare> = Vec::new();
 

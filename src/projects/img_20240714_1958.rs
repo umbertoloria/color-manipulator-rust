70ae1d851282e1b97ffad1ccec7c143aca1613b4
Umberto Loria <umberto.loria@gmail.com>
2025-03-09 00:56:25 +0100

    Using mean images for real image

--- src/projects/img_20240714_1958.rs
+++ src/projects/img_20240714_1958.rs
@@ -47,8 +47,22 @@ fn set_boundaries(scalar: f32) -> f32 {
 }
 
 pub fn img_20240714_1958() {
+    // Mean image
+    let image = load_int_file_image_layer("./input/20240714_1958.png");
+    let mut canvas = create_mean_canvas_layer_from_int_layer(&image as &dyn IntAbsLayer);
+    let mean_times = 300;
+    for i in 0..mean_times {
+        let new_canvas = create_mean_canvas_layer_from_int_layer(&canvas as &dyn IntAbsLayer);
+        canvas = new_canvas;
+        println!("Painted mean {i}");
+    }
+    create_int_filtered_layer(Box::new(canvas), |c, _, _, _| c)
+        .save(get_path_out("20240714_1958_mean_300.png"));
+
+    // Real image
     create_int_filtered_layer(
-        load_int_file_image_layer_box("./input/20240714_1958.png"),
+        // load_int_file_image_layer_box("./input/20240714_1958.png"),
+        load_int_file_image_layer_box("./out/20240714_1958_mean_300.png"),
         |c, p, width, height| {
             let c = c.to_rgb();
             let p = p.to_pos(width, height);
@@ -68,16 +82,4 @@ pub fn img_20240714_1958() {
         },
     )
     .save(get_path_out("20240714_1958.png"));
-
-    // Mean image
-    let image = load_int_file_image_layer("./input/20240714_1958.png");
-    let mut canvas = create_mean_canvas_layer_from_int_layer(&image as &dyn IntAbsLayer);
-    let mean_times = 300;
-    for i in 0..mean_times {
-        let new_canvas = create_mean_canvas_layer_from_int_layer(&canvas as &dyn IntAbsLayer);
-        canvas = new_canvas;
-        println!("Painted mean {i}");
-    }
-    create_int_filtered_layer(Box::new(canvas), |c, _, _, _| c)
-        .save(get_path_out("20240714_1958_mean_300.png"));
 }
